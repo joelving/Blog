fb70aa8eff0a40850b3584724042a1f6b5656dbc
Peter Toft Jølving <joelving@rescopeit.dk>
2022-01-04 20:34:25 +0100

    Extract inlined scripts from theme.

---
+++ themes/joelving/static/js/highlightInit.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    hljs.configure({ classPrefix: '', useBR: false });
+    $('pre.code-highlight > code, pre > code').each(function(i, block) {
+      if (!$(this).hasClass('codeblock')) {
+        $(this).addClass('codeblock');
+      }
+      hljs.highlightBlock(block);
+    });
+  });
